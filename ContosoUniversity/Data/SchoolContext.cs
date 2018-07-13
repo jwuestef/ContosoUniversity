f3b9624d2da276e091218b05542daf914e73c63f
jwuestef <jwuestef@gmail.com>
2018-07-13 18:16:55 -0400

    pausing on Friday, about 3/4 through page 1 of tutorial

---
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,41 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+
+
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+
+        //This code creates a DbSet property for each entity set.
+        //In Entity Framework terminology, an entity set typically corresponds to a database table, and an entity corresponds to a row in the table.
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        //You could've omitted the DbSet<Enrollment> and DbSet<Course> statements and it would work the same.
+        //The Entity Framework would include them implicitly because the Student entity references the Enrollment entity and the Enrollment entity references the Course entity.
+
+
+        //When the database is created, EF creates tables that have names the same as the DbSet property names.
+        //Property names for collections are typically plural (Students rather than Student), but developers disagree about whether table names should be pluralized or not.
+        //For these tutorials you'll override the default behavior by specifying singular table names in the DbContext.
+        //To do that, add:
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+
+
+    }
+}
--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoUniversity.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace ContosoUniversity
@@ -14,8 +16,31 @@ namespace ContosoUniversity
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            //Do the following on application startup:
+            //Get a database context instance from the dependency injection container.
+            //Call the seed method, passing to it the context.
+            //Dispose the context when the seed method is done.
+
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
+
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
